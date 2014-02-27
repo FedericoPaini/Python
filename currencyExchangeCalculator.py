51b0aac3ab03b9cfbeb59f918f0e7c3d07b0cb6d
Federico Paini <federico@paini.org>
2014-02-26 19:39:56 -0500

    Currency Exchange Calculator

---
+++ currencyExchangeCalculator.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+import sys, os, os.path, urllib2, re, cookielib, time, datetime, locale
+
+file = 'conv_rates.txt' #file containing the exchnage rates
+
+locale.setlocale( locale.LC_ALL, '' )
+
+def check_file(file):
+	if os.path.exists(file) == False:
+		grab_web_rates()
+
+	#check that the file is no more than 24 hours long
+	if  time.time() - os.path.getmtime(file) > 86400: #file older than 24 hours
+		#grab the currency exchange data form the internet
+		grab_web_rates()
+	else: #file is up to date
+		return create_exchange_dict()
+
+def create_exchange_dict():
+	exchange_rates_dict = {}
+	conversion_rates = open (file, 'r') #open file read only
+
+	for line in conversion_rates:
+		exchange_rates_dict[line.split(",")[0]] = line.split(",")[1]
+
+	conversion_rates.close()
+	return exchange_rates_dict
+
+def grab_web_rates():
+	conversion_rates = open (file, 'w')
+	web = urllib2.urlopen('http://www.x-rates.com/table/?from=USD')
+	html = web.read()
+	line = 1
+	titles = re.findall(r'<td class=\'rtRates\'><a href=\'/graph/\?from=(.*?)</td>', html)
+	for title in titles:
+		title = title.replace("amp;to=", "")
+		title = title.replace("&", "-")
+		title = title.replace("'>", ",")
+		title = title.replace("</a>", " ")
+		title = title.replace("  ", "")
+		title = title.rstrip()
+		conversion_rates.write(title + ' \n')
+		line += 1
+		if line >= 20:
+			break
+	conversion_rates.close()
+
+def get_result(choice, amount):
+	dict = create_exchange_dict()
+	for exchange,rate in dict.items():
+		if exchange == choice:
+			result = amount * float(rate)
+			return rate, result
+			break
+		else:
+			error = "Exchange not found! -get_result()- "
+
+def format_currency(value):
+    return "{:,.2f}".format(value)
+
+def main():
+	os.system('clear')
+	print '''
+	Chose the Conversion:
+	1. US Dollar -> Euro
+	2. US Dollar -> British Pound
+	3. US Dollar -> Canadian Dollar
+	4. US Dollar -> Chines Yuan
+	0. Exit
+	'''
+	choice = int(raw_input("\t"))
+	while choice != 0:
+		check_file(file)
+		amount = float(raw_input("Dollar Amount: $"))
+
+		if choice == 1: #Euro
+			os.system('clear')
+			choice = "USD-EUR"
+			r = get_result(choice, amount)
+			result = r[1]
+			rate = r[0]
+			e=u'\u20ac' #euro symbol
+			amount = locale.currency( amount, grouping=True )
+			print "Your amount: ", amount
+			print "Converts to: ", e, format_currency(result)
+			print "The US Dollar -> Euro exchnage rate is: ", rate
+			break
+
+		elif choice == 2: #GB
+			choice = "USD-GBP"
+			os.system('clear')
+			r = get_result(choice, amount)
+			result = r[1]
+			rate = r[0]
+			e = u'\xA3' #British Pound symbol
+			amount = locale.currency( amount, grouping=True )
+			print "Your amount: ", amount
+			print "Converts to: ", e, format_currency(result)
+			print "The US Dollar -> GBP exchnage rate is: ", rate
+			break
+
+		elif choice == 3: #Canadian Dollar
+			choice = "USD-CAD"
+			os.system('clear')
+			r = get_result(choice, amount)
+			result = r[1]
+			rate = r[0]
+			amount = locale.currency( amount, grouping=True )
+			result = locale.currency( result, grouping=True )
+			print "Your amount: ", amount
+			print "Converts to: ", result
+			print "The US Dollar -> Canadian Dollar exchnage rate is: ", rate
+			break
+
+		elif choice == 4: #Chinese Yuan
+			choice = "USD-CNY"
+			os.system('clear')
+			r = get_result(choice, amount)
+			result = r[1]
+			rate = r[0]
+			amount = locale.currency( amount, grouping=True )
+			print "Your amount: ", amount
+			print "Converts to Yuan: ", e, format_currency(result)
+			print "The US Dollar -> Chinese Yuan exchnage rate is: ", rate
+			break
+
+#execution
+main()
--- dev.py
+++ dev.py
@@ -1,20 +1,70 @@
 #!/usr/bin/python
-import random
 
-maxDivisors = 1
-numWithMax = 1
-n = 2
+import sys, os, os.path, urllib2, re, cookielib, time, datetime
 
-while n != 10001:
-	divisorCount = 0
-	for x in xrange(1,n):
-		if (n % x) == 0:
-			divisorCount +=1
+def menu():
+	os.system('clear')
+	print '''
+	Chose the Conversion:
+	1. USD -> EUR
+	2. USD -> British Pound
+	3. USD -> CAD
+	4. USD -> Chines Yuan
+	5. USD -> Australian Dollar
+	7. USD -> Indian Rupee
+	8. USD -> Swedish Krona
+	9. USD -> Norwegian Krone
+	10. USD -> Icelandic Krona
+	11. USD -> Danish Krone
+	12. USD -> Japanese Yen
+	13. USD -> Swiss Franc
+	14. USD -> Argentine Peso
+	15. USD -> Brazilian Real
+	00. Exit
+	'''
 
-		if (divisorCount > maxDivisors):
-			maxDivisors = divisorCount
-			numWithMax = n
-	n  += 1
+	choice = int(raw_input("\t"))
+	return choice
 
-print "The maximum number of divisors is ", maxDivisors 
-print "A number with max divisors is ", numWithMax
+
+def get_web_data():
+	timeStamp = datetime.datetime.now()
+	conversion_rates = open ('conv_rates.txt', 'w')
+	web = urllib2.urlopen('http://www.x-rates.com/table/?from=USD')
+	html = web.read()
+
+	try:
+		line = 1
+		titles = re.findall(r'<td class=\'rtRates\'><a href=\'/graph/\?from=(.*?)</td>', html)
+		conversion_rates.write(str(timeStamp) + '\n')
+		for title in titles:
+			title = title.replace("amp;to=", "")
+			title = title.replace("&", "-")
+			title = title.replace("'>", ",")
+			title = title.replace("</a>", " ")
+			title = title.replace("  ", "")
+			title = title.rstrip()
+			conversion_rates.write(title + ' \n')
+			line += 1
+			if line >= 20:
+				break
+		conversion_rates.close()
+
+	except Exception, e:
+		print str(e)
+
+
+# Execution
+#choice = ''
+#while choice != 00:
+#	choice = menu()
+#	if choice == 1:
+#		print "You chose 1!"
+#		break
+#	elif choice == 2:
+#		print "You chose 2!"
+#		break
+#	elif choice == 3:
+#		print "You chose 3!"
+#		break
+get_web_data()
